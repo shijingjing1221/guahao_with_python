4a57bc35975b0f3213ad9fb143a59ce3367a3d2d
Jingjing Shi <shijingjing1221@gmail.com>
2019-06-03 20:01:26 +0800

    第一个版本
    通过基本测试。待解决问题，弹出的确认框

---
+++ guahao.py
@@ -0,0 +1,167 @@
+#!/usr/bin/python
+# -*- coding: UTF8 -*-
+
+"""
+A simple selenium test example written by python
+"""
+
+
+import unittest
+import re
+import time
+import sys
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+
+import json
+
+import sys
+defaultencoding = 'utf8'
+if sys.getdefaultencoding() != defaultencoding:
+    reload(sys)
+    sys.setdefaultencoding(defaultencoding)
+
+class TestTemplate(unittest.TestCase):
+    """Include test cases on a given url"""
+
+    def setUp(self):
+        print sys.getdefaultencoding()
+        """Start web driver"""
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--no-sandbox')
+        #chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-gpu')
+        self.driver = webdriver.Chrome(chrome_options=chrome_options)
+        self.driver.implicitly_wait(100)
+        f = open("Settings.json") 
+        self.settings = json.load(f)
+
+    # def test_print_setting(self):
+    #     print "================"
+    #     print self.settings['name'].decode("utf-8")
+
+    def tearDown(self):
+        """Stop web driver"""
+        # self.driver.quit()
+
+    # def loadSettings(self):
+    #     f = open("Settings.json") 
+    #     settings = json.load(f)
+    #     return settings
+ 
+
+    def waitAndClickByXpath(self, xpath):
+         driver = self.driver
+         WebDriverWait(driver, 1000).until(
+            lambda driver: driver.find_element_by_xpath(xpath))
+         el = driver.find_element_by_xpath(xpath)
+         el.click()
+
+    def waitAndClickLastByXpath(self, xpath):
+         driver = self.driver
+         WebDriverWait(driver, 1000).until(
+            lambda driver: driver.find_elements_by_xpath(xpath))
+         elements = driver.find_elements_by_xpath(xpath)
+         length = len(elements)
+         el = elements[length - 1]
+         el.click()
+    
+    def waitAndClickByCss(self, selector):
+         driver = self.driver
+         WebDriverWait(driver, 1000).until(
+            lambda driver: self.driver.find_element_by_css_selector(selector))
+         el = self.driver.find_element_by_css_selector(selector)
+         el.click()
+
+    def waitAndClickSelectByCss(self, selector, value):
+        driver = self.driver
+        WebDriverWait(driver, 1000).until(
+            lambda driver: self.driver.find_element_by_css_selector(selector))
+        driver.find_element_by_css_selector(selector).find_element_by_xpath("//option[@value='" +value+ "']").click()
+
+
+    def waitAndSendKey(self, selector, sendKeysValue):
+         driver = self.driver 
+         WebDriverWait(driver, 1000).until(
+            lambda driver: self.driver.find_elements_by_css_selector(selector))
+         el = self.driver.find_elements_by_css_selector(selector)[0]
+         el.send_keys(sendKeysValue)
+        #   elem.send_keys(Keys.RETURN)
+
+
+    def waitAndSendKeyWithChinese(self, selector, sendKeysValue):
+         driver = self.driver 
+         WebDriverWait(driver, 1000).until(
+            lambda driver: self.driver.find_elements_by_css_selector(selector))
+         el = self.driver.find_elements_by_css_selector(selector)[0]
+         el.send_keys(sendKeysValue.decode("utf-8"))
+        #   elem.send_keys(Keys.RETURN)
+
+    def waitAndClickByLinkText(self, link_text):
+        driver = self.driver 
+        WebDriverWait(driver, 10000).until(
+            lambda driver: self.driver.find_element_by_link_text(link_text.decode("utf-8")))
+        el = driver.find_element_by_link_text(link_text.decode("utf-8"))
+        el.click()
+
+
+
+    def test_guahao(self):
+        login_phone = self.settings['login_phone']
+        login_pass = self.settings['login_pass']
+        hospital =  self.settings['hospital'].decode("utf-8")
+        department =  self.settings['department'].decode("utf-8")
+        url="http://www.114yygh.com/index.htm"
+        self.driver.get(url)  
+
+        self.waitAndClickByCss("#bdtj1")
+        self.waitAndSendKey("#mobileNo", login_phone)
+        self.waitAndClickByCss("#login_1")
+        self.waitAndClickByCss("#pwd_login")
+        self.waitAndSendKey("#pwd", login_pass)
+        self.waitAndClickByCss("#loginStep2_pwd")
+        self.waitAndSendKeyWithChinese("#words", hospital)
+        self.waitAndClickByCss(".searchBox input:nth-child(3)")
+        self.waitAndClickByLinkText(hospital)
+        self.waitAndClickByLinkText(department)
+
+        #等待今天放号码
+        youhao_xpath='//span[text()="有号"]'.decode("utf-8")
+        #driver.find_elements_by_xpath(xpath)
+        #self.waitAndClickByXpath()
+        youhao_loaded=False
+        while youhao_loaded==False:
+            youhao_el = self.driver.find_element_by_xpath(youhao_xpath)
+            if youhao_el:
+                youhao_el.click()
+                youhao_loaded = True
+            else:
+                self.driver.refresh()
+
+
+        self.waitAndClickLastByXpath('//div[@id="ksorder_djgh_doctor"]//a[text()="预约挂号"]'.decode("utf-8"))
+        self.waitAndClickSelectByCss('#Rese_db_dl_idselect', '1')
+        self.waitAndClickByCss("#send_sms_code_btn")
+        self.driver.switch_to_alert().accept();
+        self.driver.switch_to_window();
+        
+        #等待输入验证码
+        founded = False
+        while founded == False:
+            sms_el = self.driver.find_element_by_css_selector("#sms_code")
+            if sms_el:
+                sms_code = sms_el.get_attribute('value')
+                if len(sms_code) == 6:
+                    founded = True
+
+        #点击预约按钮
+        self.waitAndClickByCss("#Rese_db_qryy_btn_v1")
+
+        # self.waitAndClickByXpath('//span[text()="有号"]'.decode("utf-8"))
+
+  
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestTemplate)
+    unittest.TextTestRunner(verbosity=2).run(suite)
---
+++ installation.md
@@ -0,0 +1,16 @@
+
+# window 10下安装方法
+
+## 安装python&pip&selenium
+https://jingyan.baidu.com/article/47a29f244aec6bc014239985.html
+
+## 设置环境变量
+
+C:\Python27
+C:\Python27\Scripts
+C:\Python27\Lib\site-packages
+
+## 拷贝installation_software下面的webdriver到path下面的任意一个文件夹
+
+
+
